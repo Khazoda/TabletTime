f818c0a0408b18548f6f85ff0005ed0ba7122409
Khazoda <khazoda@gmail.com>
2018-08-27 23:04:04 +0100

    fixed main (Satome's changes)

--- main.js
+++ main.js
@@ -10,23 +10,21 @@ loadMain(loadingScreen, mainScreen);
 function loadMain(loadingScreen,mainScreen) {
     mainWin = new BrowserWindow({width: 800, minWidth: 560, height: 800, minHeight: 600, frame:false, show:false, backgroundColor: '#2e2c29'});
     mainWin.loadFile(mainScreen);
-
     loadingWin = loadSplash(loadingScreen);
     mainWin.once('ready-to-show', () => {
-        mainWin.show();
         loadingWin.hide();
         loadingWin.close(); 
+        mainWin.show();
     })
 };
 
 function  loadSplash (loadingScreen) {
-    let loadingWin = new BrowserWindow({width: 800, height: 800,transparent:true, frame:false, show:false});
-
+    let loadingWin = new BrowserWindow({width: 800, height: 800, parent:mainWin,transparent:true, frame:false, show:false});
     loadingWin.loadFile(loadingScreen);
     loadingWin.once('ready-to-show', () => {
         loadingWin.show();
-        return loadingWin;
     })
+    return loadingWin;
 };
 
 
