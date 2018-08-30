9c0bc8c37b3c49c01795bae49d84713a676bce37
Khazoda <khazoda@gmail.com>
2018-08-30 20:15:38 +0100

    document bug

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ app.on('ready', () => {
 let mainWin = null;
 const loadingScreen = 'src/loading/loading.html';
 const mainScreen = 'src/index/index.html';
+const addPillScreen = 'src/addPill/addPill.html';
+
 loadMain(loadingScreen, mainScreen);
 });
 
 function loadMain(loadingScreen,mainScreen) {
-    mainWin = new BrowserWindow({width: 800, minWidth: 560, height: 800, minHeight: 600, frame:false, show:false, backgroundColor: '#2e2c29'});
+    mainWin = new BrowserWindow({width: 800, height: 800, minWidth: 560, minHeight: 600, frame:false, show:false, backgroundColor: '#2e2c29'});
     mainWin.loadFile(mainScreen);
     loadingWin = loadSplash(loadingScreen);
     mainWin.once('ready-to-show', () => {
@@ -26,6 +28,12 @@ function  loadSplash (loadingScreen) {
     })
     return loadingWin;
 };
+
+/* Here's where the document error comes from */
+document.getElementById("add-pill-button").addEventListener("click", function(){
+    document.getElementById("demo").innerHTML = "Hello World";
+});
+
 
 
 
